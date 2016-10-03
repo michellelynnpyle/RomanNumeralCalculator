f913924ce25695db511685942345005f5e39a57d
michellelynnpyle <michellepyle@gmail.com>
2016-10-03 14:13:44 -0500

    Adding calculator code file and header file

---
+++ RomanNumeralCalculator.c
@@ -0,0 +1,6 @@
+
+/* RomanNumeralCalculator.c */
+/* Written by Michelle Lynn Pyle */
+/* October 3 2016 */
+/* this file contains a function to add or subtract two Roman Numerals */
+
---
+++ RomanNumeralCalculator.h
@@ -0,0 +1,6 @@
+
+/* RomanNumeralCalculator.h */
+/* Written by Michelle Lynn Pyle */
+/* October 3 2016 */
+/* this file contains header code for the Roman Numeral Calculator */
+
